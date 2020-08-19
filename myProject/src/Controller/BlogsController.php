dc1e5585e61c7c3ed8cda556cdbfe925981d064b
quanganh <quanganhk62@gmail.com>
2020-08-19 10:46:38 +0700

    Create link and Change layout for a controller also for all controller blogs

--- myProject/src/Controller/BlogsController.php
+++ myProject/src/Controller/BlogsController.php
@@ -1,10 +1,25 @@
 <?php
 namespace App\Controller;
+use Cake\Event\Event;
+use Cake\Event\EventInterface;
 
 class BlogsController extends AppController{
 
+    public function beforeFilter(EventInterface $event)
+    {
+        $this->viewBuilder()->setLayout('blog');
+        // debug($event);
+        // exit();
+    }
     public function home(){
-        //exit("Hello");
-        $this->viewBuilder()->setLayout('ajax');
+        //exit("Hello");     
+    }
+
+    public function about(){
+        //about go here
+    }
+
+    public function contact(){
+        // contact go here
     }
 }
--- myProject/templates/Blogs/home.php
+++ myProject/templates/Blogs/home.php
@@ -1,20 +1,48 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<div class="container">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<div class="row">
+  <div class="col-4">
+      <h3 style="color: #fff;" class="bg-info text-capitalize p-1">Recent Post</h3>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item"><a href ="#">Cras justo odio</a></li>
+        <li class="list-group-item"><a href ="#">Dapibus ac facilisis in</a></li>
+        <li class="list-group-item"><a href ="#">Morbi leo risus</a></li>
+        <li class="list-group-item"><a href ="#">Porta ac consectetur ac</a></li>
+        <li class="list-group-item"><a href ="#">Vestibulum at eros</a></li>
+      </ul>
+  </div>
 
-    <title>IBlog</title>
-  </head>
-  
-  <body>
-    <?= $this->element('nav'); ?>
-    <?= $this->element('content'); ?>
-    <footer class="bg-dark mt-3 text-light p-4 text-center">footer</footer>
+  <div class="col-8">
+      <div class="row">
+         <div class="list-group ">
+            <a href="#" class="list-group-item list-group-item-action flex-column mb-2">
+              <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">List group item heading</h5>
+                <small>3 days ago</small>
+              </div>
+              <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+              <small>Donec id elit non mi porta.</small>
+            </a>
+            <a href="#" class="list-group-item list-group-item-action flex-column mb-2">
+              <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">List group item heading</h5>
+                <small class="text-muted">3 days ago</small>
+              </div>
+              <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+              <small class="text-muted">Donec id elit non mi porta.</small>
+            </a>
+            <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+              <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">List group item heading</h5>
+                <small class="text-muted">3 days ago</small>
+              </div>
+              <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+              <small class="text-muted">Donec id elit non mi porta.</small>
+            </a>
+          </div>
+      </div>
+  </div>
+</div>
 
-  </body>
-</html>
+</div>
+
